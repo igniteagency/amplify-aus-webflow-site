de7cb5cf7934f72493aa36847ab629cf77b2232f
Sylvain Girard <sylvain97@gmail.com>
2025-10-13 13:53:44 +1100

    Remove loader code

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,9 +1,7 @@
 import { initIntroText } from '$components/intro-text';
-import { initLoader } from '$components/loader';
 
 // Run on Webflow DOM ready
 window.Webflow = window.Webflow || [];
 window.Webflow.push(() => {
-  initLoader();
-  initIntroText({ rotationMaxDeg: 6 });
+  initIntroText({ rotationMaxDeg: 2 });
 });
